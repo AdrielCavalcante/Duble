9386f6229f094034f5a069202c052bf8caaf6116
Adriel Cavalcante <adriel.cavalcante2004@gmail.com>
2022-04-21 21:25:17 -0300

    Adicionando website v1

---
+++ js/script.js
@@ -0,0 +1,3 @@
+//Data atual
+var d = new Date();
+document.getElementById("date").innerHTML = "<strong style='font-size: 11pt;'>&copy "+d.getFullYear()+" Família Dublemix. Todos os direitos reservados.</strong>";
